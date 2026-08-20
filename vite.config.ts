f59f9c0f127dc3b14d986d5951d4cc024efd10e9
Sigmabeta298 <syamavarun@yahoo.com>
2026-08-20 17:20:13 +0530

    Set up Sveltekit PWA foundation

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
 	plugins: [
 		sveltekit({
 			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter()
+		}),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'IntroVerDuce',
+				short_name: 'IntroVerDuce',
+				description: 'Introduce yourself without saying a word.',
+				start_url: '/',
+				display: 'standalone'
+			}
 		})
 	]
 });
